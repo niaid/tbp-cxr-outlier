c3f3682fe2e26256d72e55dc20b23e1b134552da
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-07-16 10:15:24 -0400

    Autodoc all sub-modules, and use sphinx_autodoc_typehints

--- docs/api.rst
+++ docs/api.rst
@@ -3,3 +3,13 @@ API
 
 .. automodule:: tbpcxr
     :members:
+
+.. automodule:: tbpcxr.model
+    :members:
+    :undoc-members:
+
+.. automodule:: tbpcxr.registration
+    :members:
+
+.. automodule:: tbpcxr.utilities
+    :members:
--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+# To get teh correct version out package needs to be installed from the source
+# directory in development or editable mode.
+#sys.path.insert(0, os.path.abspath('..'))
+
 
 
 # -- Project information -----------------------------------------------------
@@ -33,6 +35,7 @@ author = 'Bradley Lowekamp'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx_click.ext',
+    'sphinx_autodoc_typehints',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
