1756bd975f1ab659e5a9de543183f3decf94d21c
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-07-21 16:29:55 -0400

    Refactor PCAModel with base class: Model
    The PCAModel class is a specific instance of a model representation,
    and an outlier classifier. The base class: Model, provides an
    abstraction for this type of class. A general load_model class method
    is provided in Model, which can load any class derived from Model. The
    Model class also provides the common functionality for registering an
    image to the atlas and transforming images to a reference image
    coordinates.

--- test/test_model.py
+++ test/test_model.py
@@ -1,4 +1,4 @@
-from tbpcxr.model import PCAModel
+from tbpcxr.model import PCAModel, Model
 from tbpcxr.utilities import normalize_img
 import SimpleITK as sitk
 import pytest
@@ -7,15 +7,15 @@ import os.path
 
 @pytest.fixture
 def outlier_pcamodel():
-    return PCAModel.load_outlier_pcamodel()
+    return Model.load_outlier_pcamodel()
 
 
 def test_load_outlier_pcamodel():
-    model = PCAModel.load_outlier_pcamodel()
+    model = Model.load_outlier_pcamodel()
 
     assert isinstance(model.image_atlas, sitk.Image)
-    assert isinstance(model.image_ref, sitk.Image)
-    assert all([s == PCAModel.SAMPLE_IMAGE_SIZE for s in model.image_ref.GetSize()])
+    assert isinstance(model.image_reference, sitk.Image)
+    assert all([s == model.reference_size for s in model.image_reference.GetSize()])
     assert model.pca is not None
     assert model.outlier_detector is not None
 
@@ -39,9 +39,7 @@ def test_A(filename, outlier_class, outlier_pcamodel):
 
     img = sitk.ReadImage(path_to_file, sitk.sitkFloat32)
 
-    nimg = normalize_img(img)
+    arr = outlier_pcamodel.to_observations([img])
 
-    rimg = outlier_pcamodel.register_to_atlas_and_resample(nimg, verbose=0)
-    arr = outlier_pcamodel._images_to_arr([rimg])
 
     assert outlier_pcamodel.outlier_predictor(arr)[0] == outlier_class
