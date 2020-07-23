d2b2a2c820770de3f7ae917f5717a177ddd9820f
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-07-23 13:40:55 -0400

    Add testing for Model.load_model

--- test/test_model.py
+++ test/test_model.py
@@ -1,9 +1,12 @@
 from tbpcxr.model import PCAModel, Model
 from tbpcxr.utilities import normalize_img
+import tbpcxr
 import SimpleITK as sitk
 import pytest
 import os.path
 
+path_to_current_file = os.path.realpath(__file__)
+current_directory = os.path.dirname(path_to_current_file)
 
 @pytest.fixture
 def outlier_pcamodel():
@@ -19,6 +22,23 @@ def test_load_outlier_pcamodel():
     assert model.pca is not None
     assert model.outlier_detector is not None
 
+model_fname = [
+    "cxr-test-06c.pkl"
+]
+@pytest.mark.parametrize("model_name",
+                         tbpcxr.model_list +
+                         [os.path.join(current_directory, "data", fn) for fn in model_fname])
+def test_load_model(model_name):
+    model = Model.load_model(model_name)
+
+    assert isinstance(model.image_atlas, sitk.Image)
+    assert isinstance(model.image_reference, sitk.Image)
+    assert all([s == model.reference_size for s in model.image_reference.GetSize()])
+    assert model.pca is not None
+    assert model.outlier_detector is not None
+
+    img = model.register_to_atlas_and_resample(sitk.Noise(model.image_atlas))
+    assert all([s == model.reference_size for s in img.GetSize()])
 
 data_fname = [
     "00000041_005.png",
@@ -33,13 +53,10 @@ data_fname = [
                          [pytest.param(fn, 1) for fn in data_fname])
 def test_A(filename, outlier_class, outlier_pcamodel):
 
-    path_to_current_file = os.path.realpath(__file__)
-    current_directory = os.path.dirname(path_to_current_file)
     path_to_file = os.path.join(current_directory, "data", filename)
 
     img = sitk.ReadImage(path_to_file, sitk.sitkFloat32)
 
     arr = outlier_pcamodel.to_observations([img])
-
 
     assert outlier_pcamodel.outlier_predictor(arr)[0] == outlier_class
