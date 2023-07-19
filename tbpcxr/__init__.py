465f5b4beea22f31b0bc0ec8753511bff931a77f
Bradley Lowekamp <blowekamp@mail.nih.gov>
2023-07-19 10:55:05 -0400

    Update GHA for pyproject.toml usage

--- tbpcxr/__init__.py
+++ tbpcxr/__init__.py
@@ -2,20 +2,9 @@ import pkg_resources as res
 import os
 
 try:
-    from importlib.metadata import version, PackageNotFoundError
-
-    __version__ = version(__name__)
+    from ._version import version as __version__
 except ImportError:
-    from pkg_resources import get_distribution, DistributionNotFound
-
-    try:
-        __version__ = get_distribution(__name__).version
-    except DistributionNotFound:
-        # package is not installed
-        pass
-except PackageNotFoundError:
-    # package is not installed
-    pass
+    __version__ = "unknown"
 
 
 model_list = [os.path.splitext(fn)[0] for fn in res.resource_listdir(__name__, "model")]
