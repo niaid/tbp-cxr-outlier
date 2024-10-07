82c3175132794538824f57c6660650e79aea42cd
Bradley Lowekamp <blowekamp@mail.nih.gov>
2024-10-07 14:27:41 -0400

    Migrate to importlib.resources over pkg_resources
    The latter has been deprecated and removed in python 3.12.

--- tbpcxr/__init__.py
+++ tbpcxr/__init__.py
@@ -1,14 +1,9 @@
-import pkg_resources as res
-import os
+from .data import model_list
 
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
 
-
-model_list = [os.path.splitext(fn)[0] for fn in res.resource_listdir(__name__, "model")]
-
-del os
-
+__all__ = ["model_list"]
 __author__ = ["Bradley Lowekamp"]
